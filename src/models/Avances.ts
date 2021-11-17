89650d2600fab8f5d8653963fa3436b0cd74f797
Einer <einermedina15@gmail.com>
2021-11-17 11:20:41 -0500

    CRUD Avances

---
+++ src/models/Avances.ts
@@ -0,0 +1,41 @@
+import { Schema, model } from 'mongoose';
+import { Proyectos } from './Proyectos';
+//import { Usuarios } from './Usuarios';
+
+interface Avances {
+    proyecto: Schema.Types.ObjectId;
+    fecha: Date;
+    descripcion: string;
+    observaciones: [string];
+    //   creadoPor: Schema.Types.ObjectId;
+}
+
+const avancesSchema = new Schema<Avances>({
+    fecha: {
+        type: Date,
+        required: true,
+    },
+    descripcion: {
+        type: String,
+        required: true,
+    },
+    observaciones: [
+        {
+            type: String,
+        },
+    ],
+    proyecto: {
+        type: Schema.Types.ObjectId,
+        ref: Proyectos,
+        required: true,
+    },
+    //   creadoPor: {
+    //     type: Schema.Types.ObjectId,
+    //     //ref: Usuarios,
+    //     required: true,
+    //   },
+});
+
+const Avances = model('Avances', avancesSchema, 'Avances');
+
+export { Avances };
