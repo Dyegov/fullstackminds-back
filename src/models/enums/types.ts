302fa529a199f0cd7410ca85f9a1f7815342ec34
Dyegov <diego.valencr@gmail.com>
2021-12-01 17:48:57 -0500

    Agregar enums Proyecto gql

--- src/models/enums/types.ts
+++ src/models/enums/types.ts
@@ -1,10 +1,22 @@
 import { gql } from 'apollo-server-express';
 
 const tiposEnums = gql`
-    enum Enum_EstadoInscripcion {
-        ACEPTADA
-        RECHAZADA
-        PENDIENTE
+  enum Enum_EstadoProyecto {
+    ACTIVO
+    INACTIVO
+  }
+
+  enum Enum_FaseProyecto {
+    INICIADO
+    DESARROLLO
+    TERMINADO
+    NULA
+  }
+
+  enum Enum_EstadoInscripcion {
+    ACEPTADA
+    RECHAZADA
+    PENDIENTE
   }
 `;
 
